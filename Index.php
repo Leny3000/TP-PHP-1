30604a58911c695c43738b2da44e1740a750233d
unknown <l.guillard@SWSIO.LOCAL>
2025-04-10 15:11:12 +0200

    ajout fichier

---
+++ Index.php
@@ -0,0 +1,30 @@
+<?php
+// Définition d'une classe Personne
+class Personne {
+    // Propriétés (attributs)
+    private $nom;
+    private $prenom;
+    private $age;
+    
+    // Constructeur
+
+    
+    // Getters (accesseurs)
+    
+    // Setters (mutateurs)
+
+    
+    // Méthodes
+    public function sePresenter() {
+      
+    }
+    
+    public function estMajeur() {
+
+    }
+
+// Utilisation de la classe
+
+
+// Modification des propriétés
+}
