1d70cdbbfecd8b9be5798ddca25af4cdb8418202
unknown <l.guillard@SWSIO.LOCAL>
2025-04-15 10:56:03 +0200

    modif

--- Index.php
+++ Index.php
@@ -7,13 +7,42 @@ class Personne {
     private $age;
     
     // Constructeur
-
+    public function __construct($nom, $prenom, $age)
+    {
+        echo 'Voici le constructeur !';
+        $this -> setNom($nom);
+        $this -> setPrenom($prenom);
+        $this -> setAge($age);
+    }
     
     // Getters (accesseurs)
     
+    public function getNom() {
+      return $this->nom;
+    }
+
+    public function getPrenom() {
+      return $this->prenom;
+    }
+
+    public function getAge() {
+      return $this->age;
+    }
+
     // Setters (mutateurs)
 
-    
+   public function setNom() {
+      $this->nom = $nom;
+   }
+
+   public function setPrenom() {
+      $this->prenom = $prenom;
+   }
+
+   public function setAge() {
+      $this->nom = $nom;
+   }
+   
     // Méthodes
     public function sePresenter() {
       
@@ -28,3 +57,5 @@ class Personne {
 
 // Modification des propriétés
 }
+
+$Lény = new Personne("Guillard", "Lény", 19);
